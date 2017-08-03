46912543773eb38ebc6a16cf9f2e929543bd109c
Nicole <nicole.tempas@gmail.com>
2017-08-03 16:06:14 +0000

    User Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # Without @ new variable every time method is called (want @ to make faster)
+    # Whenever want to access user want to call ^ method.
+    # After ||= finding user corresponding to current session
+  end
+  helper_method :current_user
+  # helper_method in Rails is method you can access in controller and views.
+  # If you look in _top_nav.html.erb, calling this method
+
+
+  def authorize
+    redirect_to '/login' unless @current_user
+    end
+
   private
 
   def cart
