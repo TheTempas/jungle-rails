6d48085b4c84350e4fc368aa3a35ce8ab21fd746
Nicole <nicole.tempas@gmail.com>
2017-08-07 16:07:08 +0000

    RSpec test save if name, price, quantity, category all present

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    before do
+      @category = Category.new(name: "Test Category")
+    end
+
+    it "Should not save if there is no name" do
+      @product = Product.new(
+        price: 5,
+        quantity: 10,
+        category: @category
+        )
+      expect(@product.valid?).to be(false)
+    end
+
+    it "Should not save if there is no price" do
+      @product = Product.new(
+        name: "Test Product",
+        quantity: 10,
+        category: @category
+        )
+      expect(@product.valid?).to be(false)
+    end
+
+    it "Should not save if there is no quantity" do
+      @product = Product.new(
+        name: "Test Product",
+        price: 5,
+        category: @category
+        )
+      expect(@product.valid?).to be(false)
+    end
+
+    it "Should not save if there is no category" do
+      @product = Product.new(
+        name: "Test Product",
+        price: 5,
+        quantity: 10
+        )
+      expect(@product.valid?).to be(false)
+    end
+
+    it "Should save correctly when all fields are present" do
+      @product = Product.new(
+        name: "Test Product",
+        price: 5,
+        quantity: 10,
+        category: @category
+        )
+      expect(@product.valid?).to be(true)
+    end
+
+  end
+end
